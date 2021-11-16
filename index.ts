7d73554f9dd3523f47abfa3655feff8f1e85c934
maelstonly <92147460+maelstonly@users.noreply.github.com>
2021-11-16 14:19:28 +0100

    Update index.ts

--- index.ts
+++ index.ts
@@ -72,6 +72,7 @@ export const setupPlugin: RedshiftPlugin['setupPlugin'] = async (meta) => {
 
     global.sanitizedTableName = sanitizeSqlIdentifier(config.tableName)
 
+    /*
     const queryError = await executeQuery(
         `CREATE TABLE IF NOT EXISTS public.${global.sanitizedTableName} (
             uuid varchar(200),
@@ -89,7 +90,7 @@ export const setupPlugin: RedshiftPlugin['setupPlugin'] = async (meta) => {
         [],
         config
     )
-
+    */
     if (queryError) {
         throw new Error(`Unable to connect to Redshift cluster and create table with error: ${queryError.message}`)
     }
